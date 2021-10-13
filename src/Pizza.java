0addf158ff672b4e883d09002ff9ac432d30b3e7
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2021-10-13 13:08:14 +0200

    jens' menuværktøj

--- src/Pizza.java
+++ src/Pizza.java
@@ -4,11 +4,23 @@ public class Pizza {
     private String pizzaToppings;
     private int pizzaPrice;
 
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "pizzaID=" + pizzaID +
+                ", pizzaName='" + pizzaName + '\'' +
+                ", pizzaToppings='" + pizzaToppings + '\'' +
+                ", pizzaPrice=" + pizzaPrice +
+                '}';
+    }
+
     public Pizza(int pizzaID, String pizzaName, String pizzaToppings, int pizzaPrice) {
         this.pizzaID = pizzaID;
         this.pizzaName = pizzaName;
         this.pizzaToppings = pizzaToppings;
         this.pizzaPrice = pizzaPrice;
+
+
     }
 }
 
